8b83cbdb014dbc23cec4b037ecdce29cfb036a45
RaunG <raung@icloud.com>
2019-03-25 00:30:19 -0500

    commit origin

---
+++ client/src/components/Map.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect, useContext } from 'react';
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from './PinIcon';
+import Blog from './Blog';
+import Context from '../context';
+
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
+const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context);
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
+
+  const handleMapClick = ({ lnglat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: 'CREATE_DRAFT' });
+    }
+    const [longitude, latitude] = lnglat;
+    dispatch({
+      type: 'UPDATE_DRAFT_LOCATION',
+      payload: { longitude, latitude }
+    });
+  };
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoicmF1bmdyZWVuIiwiYSI6ImNqdG5iNjE3dTBmYWg0M3FrYWhhcXJyZGMifQ.Q9dkH1EjqD0lg4QBrNxGXA"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        onClick={handleMapClick}
+        {...viewport}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
+
+        {/* Draft Pin */}
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
+          </Marker>
+        )}
+      </ReactMapGL>
+
+      {/* Blog Area to add Pin Content */}
+      <Blog />
+    </div>
+  );
+};
+
+const styles = {
+  root: {
+    display: 'flex'
+  },
+  rootMobile: {
+    display: 'flex',
+    flexDirection: 'column-reverse'
+  },
+  navigationControl: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    margin: '1em'
+  },
+  deleteIcon: {
+    color: 'red'
+  },
+  popupImage: {
+    padding: '0.4em',
+    height: 200,
+    width: 200,
+    objectFit: 'cover'
+  },
+  popupTab: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  }
+};
+
+export default withStyles(styles)(Map);
---
+++ client/src/components/Pin/CreatePin.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhotoTwoTone';
+import LandscapeIcon from '@material-ui/icons/LandscapeOutlined';
+import ClearIcon from '@material-ui/icons/Clear';
+import SaveIcon from '@material-ui/icons/SaveTwoTone';
+
+const CreatePin = ({ classes }) => {
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin a Location
+      </Typography>
+      <div>
+        <TextField name="title" label="Title" placeholder="Insert pin title" />
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+        />
+        <label htmlFor="image">
+          <Button component="span" size="small" className={classes.button}>
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          margin="normal"
+          fullwidth
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <Button className={classes.button} variant="contained" color="primary">
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+const styles = theme => ({
+  form: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingBottom: theme.spacing.unit
+  },
+  contentField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '95%'
+  },
+  input: {
+    display: 'none'
+  },
+  alignCenter: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  iconLarge: {
+    fontSize: 40,
+    marginRight: theme.spacing.unit
+  },
+  leftIcon: {
+    fontSize: 20,
+    marginRight: theme.spacing.unit
+  },
+  rightIcon: {
+    fontSize: 20,
+    marginLeft: theme.spacing.unit
+  },
+  button: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit,
+    marginLeft: 0
+  }
+});
+
+export default withStyles(styles)(CreatePin);
